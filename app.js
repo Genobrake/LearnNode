0fc016c629108815be3848624b7c38d007ac5686
Ramin Panduro <rpandurb@gmail.com>
2019-04-23 17:17:30 -0500

    Se trabajo con flechas y promesas

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-console.log('Hola Puto mundo');
-alert('Ramincito was here');
+//console.log('Hola Puto mundo');
+//alert('Ramincito was here');
+/**Asignación de Parametrors*/
+// function PruebaAngular(parametro1 : string, parametro2 : string, parametro3 : string){            //Caso 1
+// function PruebaAngular(parametro1 : string, parametro2 : string = "Joder", parametro3? : string){ //Caso 2
+//     console.log("Cadena 1: " + parametro1);
+//     console.log("Cadena 2: " + parametro2);
+//     console.log("Cadena 3: " + parametro3);
+// }
+//PruebaAngular ("Ramin", "Puto", "Panduro");       //Caso 1
+// PruebaAngular("Ramin")                           //Caso 2
+/**Funciones de Flecha vs Funciones tradicionales**/
+//Función Tradicional
+// var sumarNumeros = function (parametro1: number, parametro2: number){
+//     return parametro1 + parametro2;
+// }
+// let resultado = sumarNumeros(100, 200);
+// console.log ("Resultado: " + resultado);
+//Función de Flecha
+// var sumarNumeros = (parametro1: number, parametro2: number) =>{
+//     return parametro1 + parametro2;
+// }
+// let resultado = sumarNumeros(150, 250);
+// console.log ("Resultado: " + resultado);
+/**Promesas en ES**/
+var mipromesa = new Promise(function (resolve, reject) {
+    //resolve();    //Se ejecuta Finalizó correctamente
+    reject(); //Se ejecuta finalizó con error
+});
+mipromesa.then(function () {
+    console.log('La operación de la promesa finalizó correctamente');
+}, function () {
+    console.log('La operación de la promesa finalizó con error');
+});
--- app.ts
+++ app.ts
@@ -1,3 +1,42 @@
-console.log('Hola Puto mundo');
+//console.log('Hola Puto mundo');
+//alert('Ramincito was here');
 
-alert('Ramincito was here');
+/**Asignación de Parametrors*/
+// function PruebaAngular(parametro1 : string, parametro2 : string, parametro3 : string){            //Caso 1
+// function PruebaAngular(parametro1 : string, parametro2 : string = "Joder", parametro3? : string){ //Caso 2
+//     console.log("Cadena 1: " + parametro1);
+//     console.log("Cadena 2: " + parametro2);
+//     console.log("Cadena 3: " + parametro3);
+// }
+
+//PruebaAngular ("Ramin", "Puto", "Panduro");       //Caso 1
+// PruebaAngular("Ramin")                           //Caso 2
+
+/**Funciones de Flecha vs Funciones tradicionales**/
+//Función Tradicional
+// var sumarNumeros = function (parametro1: number, parametro2: number){
+//     return parametro1 + parametro2;
+// }
+
+// let resultado = sumarNumeros(100, 200);
+// console.log ("Resultado: " + resultado);
+
+//Función de Flecha
+// var sumarNumeros = (parametro1: number, parametro2: number) =>{
+//     return parametro1 + parametro2;
+// }
+
+// let resultado = sumarNumeros(150, 250);
+// console.log ("Resultado: " + resultado);
+
+/**Promesas en ES**/
+let mipromesa = new Promise(function(resolve: any, reject: any){
+    //resolve();    //Se ejecuta Finalizó correctamente
+    reject()        //Se ejecuta finalizó con error
+});
+
+mipromesa.then(function() {
+    console.log ('La operación de la promesa finalizó correctamente');
+}, function() {
+    console.log ('La operación de la promesa finalizó con error');
+});
