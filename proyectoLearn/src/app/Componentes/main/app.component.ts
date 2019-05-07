2b0bd6293a28ce93551e8bb401dd4987a3c97722
Ramin Panduro <rpandurb@gmail.com>
2019-05-07 12:42:36 -0500

    Creación de la clase y su llamada en el HTML

--- proyectoLearn/src/app/Componentes/main/app.component.ts
+++ proyectoLearn/src/app/Componentes/main/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Producto } from '../../Clases/Producto';
 
 @Component({
+  moduleId: module.id,  //Agregado según tutorial para mostrar HTML, funciona normal sin esto.
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// export class AppComponent {
+//   title = 'proyectoLearn';
+// }
 export class AppComponent {
-  title = 'proyectoLearn';
+  producto: Producto = new Producto(1, 'Patatas', 1, 1, 'Papas para freir');
 }
